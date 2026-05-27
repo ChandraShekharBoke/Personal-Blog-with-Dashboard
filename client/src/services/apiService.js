7982acb336b6d070d30e2d845a4a8500b65bec96
Chandrashekhar Boke <2003chetanboke@gmail.com>
2026-05-27 18:10:16 +0530

    fix: axios baseURL from env

--- client/src/services/apiService.js
+++ client/src/services/apiService.js
@@ -1,20 +1,15 @@
 import axios from 'axios';
 
 const apiService = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: import.meta.env.VITE_API_URL,
 });
 
 apiService.interceptors.request.use(
-    (config) =>{
-
+    (config) => {
         const token = localStorage.getItem('token');
-        console.log('TOKEN:', token); // ← ye add karo
-        console.log('URL:', config.url); // ← ye bhi
-
         if(token){
             config.headers['Authorization'] = `Bearer ${token}`;
         }
-        
         return config;
     }, 
     error => {
